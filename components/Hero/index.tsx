c72ad659b3bfa19793b33b6c6df75ef1b346214c
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-07-27 10:47:34 +0000

    update hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,56 +1,27 @@
 "use client";
 import Image from "next/image";
+import TypingEffect from 'react-typing-effect';
 import { useState } from "react";
 
 const Hero = () => {
   const [email, setEmail] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
   return (
     <>
       <section className="overflow-hidden pb-20 pt-35 md:pt-40 xl:pb-25 xl:pt-46">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-          <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
-            <div className=" md:w-1/2">
+
+          <div className="flex flex-col lg:flex-row lg:items-center lg:gap-8 xl:gap-32.5">
+            <div className="w-full md:w-1/2 items-center align-middle text-center">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                {/* 🔥 Solid - A Complete SaaS Web Template */}
+                <TypingEffect text="Hey 🔥" speed={100} />
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                SHRISHTI  REALTORS {"   "}
+                <TypingEffect text="I'am Saurabh Welcome to my portfolio." speed={100} />
               </h1>
-              <p>
-                Extraordinary Reach, Extraordinary Results
-              </p>
-
-              <div className="mt-10">
-                <form onSubmit={handleSubmit}>
-                  <div className="flex flex-wrap gap-5">
-                    <input
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="text"
-                      placeholder="Enter your email address"
-                      className="rounded-full border border-stroke px-6 py-2.5 shadow-solid-2 focus:border-primary focus:outline-none dark:border-strokedark dark:bg-black dark:shadow-none dark:focus:border-primary"
-                    />
-                    <button
-                      aria-label="get started button"
-                      className="flex rounded-full bg-black px-7.5 py-2.5 text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark dark:hover:bg-blackho"
-                    >
-                      Get Started
-                    </button>
-                  </div>
-                </form>
-
-                <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
-                </p>
-              </div>
             </div>
 
-            <div className="animate_right hidden md:w-1/2 lg:block">
+            <div className="w-full md:w-1/2 animate_right lg:block">
               <div className="relative 2xl:-mr-7.5">
                 <Image
                   src="/images/shape/shape-01.png"
@@ -73,25 +44,18 @@ const Hero = () => {
                   height={21.66}
                   className="absolute -right-6.5 bottom-0 z-1"
                 />
-                <div className=" relative aspect-[700/444] w-full">
-                  {/* <iframe 
-                  className="rounded-2xl"
-                    width="560" 
-                    height="315" 
-                    src="https://www.youtube.com/embed/pPl3ZZdTP3g?si=jhmBq7s2BRw780Bv" 
-                    title="YouTube video player" 
-                    frameBorder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>                      
-                    </iframe> */}
-                    <div className="relative p-1 bg-white rounded-lg shadow-lg">
-      <div className="absolute inset-0 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-lg blur-sm anima" />
-      <div className="relative z-50 bg-white h-96 rounded-lg p-6">
-        <h2 className="text-2xl font-bold mb-2">Animated Card</h2>
-        <p className="text-gray-700">This is an animated card border using Tailwind CSS.</p>
-      </div>
-    </div>
-
+                <div className="relative aspect-[700/444] w-full">
+                  <div className="relative p-1 bg-white rounded-lg shadow-lg">
+                    <div className="absolute inset-0 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-lg blur-sm anima" />
+                    
+                    <div className="relative z-0 bg-white h-96 rounded-lg p-6">
+                      <div className = "m-[5%] z-50 h-96 w-64 bg-gray-700">
+                        <h2 className="text-2xl font-bold mb-2">Animated Card</h2>
+                        <p className="text-gray-700">This is an animated card border using Tailwind CSS.</p>
+                      </div>
+                    </div>
+                  
+                  </div>
                 </div>
               </div>
             </div>
@@ -99,6 +63,7 @@ const Hero = () => {
         </div>
       </section>
     </>
+
   );
 };
 
